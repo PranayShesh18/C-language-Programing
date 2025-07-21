51bc68a80faf3973642919c1f9ff0aed77b33e23
PranayShesh18 <pranayshesh18@gmai.com>
2025-07-21 22:21:48 +0530

    Your message for submodule changes

---
+++ Arithmetic.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    int sum, div, mul, minu, mod ;
+
+    printf("Enter the first number");
+    scanf("%d",&a);
+
+    printf("Enter the second number");
+    scanf("%d",&b);
+
+    sum=a+b;
+    printf("Sum the numbers is:%d\n",sum);
+
+    minu=a-b;
+    printf("Sum the numbers is:%d\n",minu);
+
+    div=a/b;
+    printf("Sum the numbers is:%d\n",div);
+
+    mul=a*b;
+    printf("Sum the numbers is:%d\n",mul);
+
+    mod=a%b;
+    printf("Sum the numbers is:%d",mod);
+
+    return 0;
+}
